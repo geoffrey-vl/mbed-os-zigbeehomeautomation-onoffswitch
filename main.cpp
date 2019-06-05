470eeb748d60dfb4d0162e5fd12117fdef683e89
geoffrey <geoffrey.vl@gmail.com>
2019-06-05 21:28:57 +0200

    using wakeup from sleep functionality in xbeelib

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/**
+ * 
+ * Notes:
+ * Make sure to put zigbee nodes radio always on. Devices that are sleeping will also not respond to UART data
+ */ 
+
+
 #include "mbed.h"
 #include "XBeeLib.h"
 #if defined(ENABLE_LOGGING)
@@ -5,8 +12,8 @@
 using namespace DigiLog;
 #endif
 
-DigitalOut ledOk(LED1);
-DigitalOut ledErr(LED3);
+DigitalOut ledGreen(LED1);
+DigitalOut ledRed(LED3);
  
 
 Serial* log_serial;
@@ -14,24 +21,29 @@ Serial* log_serial;
 using namespace XBeeLib;
 
 int main() {
-    ledOk = 1;
-    /* Open and configure a new serial port on the USB connected to the PC */
+    ledGreen = 1;
+
+    // Open and configure a new serial port on the USB connected to the PC 
     log_serial = new Serial(STDIO_UART_TX, STDIO_UART_RX, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
     log_serial->printf("#######################" XB_LIB_BANNER);
  
 #if defined(ENABLE_LOGGING)
-    /* Create a DigiLogger object, tell it to use the opened serial port and initially set LogLevel to LogLevelDebug */
+    // Create a DigiLogger object, tell it to use the opened serial port and initially set LogLevel to LogLevelDebug 
     DigiLoggerMbedSerial *logger = new DigiLoggerMbedSerial(log_serial, LogLevelDebug); 
 #endif
 
     XBeeZB xbee = XBeeZB();
     RadioStatus initResult = xbee.init();
     if(initResult == Success) {
-        digi_log(LogLevelInfo, "Init done!\r\n");
+        xbee.set_DIO("D1", true);
+        int16_t hwversion = xbee.get_hw_version();
+        int16_t fwversion = xbee.get_fw_version();
+        digi_log(LogLevelInfo, "Init done! HW=%d FW=%d\r\n", hwversion, fwversion);
     }
     else {
-        ledErr = 1;
+        ledRed = 1;
         digi_log(LogLevelError, "Init failed!\r\n");
+        //exit(-1);
     }
 
     uint32_t val;
@@ -41,8 +53,10 @@ int main() {
         digi_log(LogLevelInfo, "MY=%lu\r\n", val);
     }
     else {
-        ledErr = 1;
+        ledRed = 1;
         digi_log(LogLevelError, "MY FAILED with %d\r\n", (int) cmdresp);
     } 
-    ledOk = 0;
+    ledGreen = 0;
+    xbee.set_DIO("D1", false);
 }
+
